8c6d76bafd695e34c4e2f332c11657f4bf660cf5
Fernando Souza <fernando.souza@crossengage.io>
2019-01-25 14:10:57 +0100

    refactoring and showing status bar messages for scripts

--- src/extension.ts
+++ src/extension.ts
@@ -4,40 +4,35 @@ import * as vscode from 'vscode';
 
 let terminal = vscode.window.createTerminal('XE', '/bin/zsh');
 
+const startFastItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1);
+startFastItem.text = 'XE.startfast';
+startFastItem.command = 'extension.crossengage.startfast';
+
+const startItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1);
+startItem.text = 'XE.start';
+startItem.command = 'extension.crossengage.start';
+
+const showTerminalItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1);
+showTerminalItem.text = 'XE.showterminal';
+showTerminalItem.command = 'extension.crossengage.showTerminal';
+
+function showStatus(message: string) {
+	const status = vscode.window.setStatusBarMessage('yarn start:fast has started');
+	const timeOut = setTimeout(() => {
+		status.dispose();
+		clearTimeout(timeOut);
+	}, 5000);
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	const startFastItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1);
-	startFastItem.text = 'XE.startfast';
-	startFastItem.command = 'extension.crossengage.startfast';
-
-	const startItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1);
-	startItem.text = 'XE.start';
-	startItem.command = 'extension.crossengage.start';
-
-	const showTerminalItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1);
-	showTerminalItem.text = 'XE.showterminal';
-	showTerminalItem.command = 'extension.crossengage.showTerminal';
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "crossengage" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.crossengage.crossengage', () => {
-		startFastItem.show();
-		startItem.show();
-		showTerminalItem.show();
-	});
-
 	let runStartFast = vscode.commands.registerCommand('extension.crossengage.startfast', () => {
 		terminal.hide();
 		terminal.dispose();
 		terminal = vscode.window.createTerminal('XE', '/bin/zsh');
 		terminal.sendText('yarn start:fast');
+		showStatus('yarn start:fast has started');
 	});
 
 	let runStart = vscode.commands.registerCommand('extension.crossengage.start', () => {
@@ -45,18 +40,21 @@ export function activate(context: vscode.ExtensionContext) {
 		terminal.dispose();
 		terminal = vscode.window.createTerminal('XE', '/bin/zsh');
 		terminal.sendText('yarn start');
+		showStatus('yarn start has started');
 	});
 
 	let showTerminal = vscode.commands.registerCommand('extension.crossengage.showTerminal', () => {
 		terminal.show();
 	});
 
-	context.subscriptions.push(disposable);
 	context.subscriptions.push(showTerminal);
 	context.subscriptions.push(runStartFast);
 	context.subscriptions.push(runStart);
 	vscode.commands.executeCommand('extension.crossengage.crossengage');
 
+	startFastItem.show();
+	startItem.show();
+	showTerminalItem.show();
 }
 
 // this method is called when your extension is deactivated
